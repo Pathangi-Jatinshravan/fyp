b2315d16e2244eb67de4ab0ebef9f0fe6c300fe7
jatin <jatinshravan@gmail.com>
2017-02-18 22:57:03 +0800

    Parsing cipher suite length

--- jatinhyper.c
+++ jatinhyper.c
@@ -2,20 +2,8 @@
 
 #define BUFFER_SIZE 52
 
-char* decode(char data[]) {
-	int i;
-	char *res = kmalloc(2*BUFFER_SIZE + 1, GFP_KERNEL);
-	char *ptr = res;
-	
-	for (i=0; i<BUFFER_SIZE; i++) {
-		ptr += sprintf(ptr, "%02X", res[i]);
-	}
-	
-	sprintf(ptr, "\n");
-	*(ptr + 1) = '\0';
+#define TLS_RSA_WITH_RC4_128_SHA "0005"
 
-	return res;	
-}
 /*
 bool hello_check(unsigned char *data, unsigned char *tail) {
 	unsigned char HANDSHAKE[] = {1, 6, 0, 3};
@@ -44,16 +32,16 @@ bool hello_check(unsigned char *data, unsigned char *tail) {
 	return false;	
 } 
 */
+
 void print_packet(char *msg, struct sk_buff *skb) {
 	struct tcphdr *tcph;
 	struct iphdr *ip_header;
 	struct ethhdr *eth;
 	char *it, *user_data, *tail;
 	char hex_data[10];
+	bool client_hello;
+	char client_cipher_suite_length[2];
 	unsigned int counter = 0;
-	unsigned int index = 0;
-	int j=0;
-	
 	const char *HANDSHAKE[2];
 	HANDSHAKE[0] = "16"; HANDSHAKE[1] = "03";
 
@@ -75,21 +63,30 @@ void print_packet(char *msg, struct sk_buff *skb) {
 	for (it=user_data; it!=tail; it++) {
 		char c = *(char *) it;
 		sprintf(hex_data, "%02X", c);
-
+		//printk("%d ", strlen(hex_data));
+		/*
 		if (c == '\0') {
 			break;
 		} 
-				
+		*/
+		
 		if (counter <= 1 && !strcmp(hex_data, HANDSHAKE[counter])) {		
 			counter++;		
 		} else if (counter <=1 && strcmp(hex_data, HANDSHAKE[counter])){
 			printk("print_packet: Not a handshake message\n");
 			break;
 		} else if (counter >1) {
-			printk("print_packet: Handshake found %s.", hex_data);
-			break;		
+			
+			if (counter == 5) {
+				if (!strcmp(hex_data, "01")) {
+					client_hello = true;
+				}
+			} else if (counter == 45 && client_hello) {
+				strcpy(client_cipher_suite_length, hex_data);
+				printk("Client Cipher suites length: %s\n", client_cipher_suite_length);						
+			}
+			counter++;	
 		}
-		
 		printk("%02X ", c);
 	
 	}
